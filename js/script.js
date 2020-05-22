ce66e14efe2e1f6856a9b305debeef68efbc84a6
vishal.dogra183 <dogravishal183@gmail.com>
2020-05-23 00:53:22 +0530

    third commit

--- js/script.js
+++ js/script.js
@@ -1,3 +1,26 @@
-$(".AppToggle").click(function(){
-$(".AppToggle,.AppMenu").toggleClass('active');
+
+
+$(document).ready(function(){
+  new WOW().init();
+
+  $(".AppToggle").click(function(){
+  $(".AppToggle,.AppMenu").toggleClass('active');
+  });
+
+  $(window).scroll(function() {
+    var height = $(window).scrollTop();
+    if (height > 100) {
+        $('#back2Top').fadeIn();
+    } else {
+        $('#back2Top').fadeOut();
+    }
 });
+$(document).ready(function() {
+    $("#back2Top").click(function(event) {
+        event.preventDefault();
+        $("html, body").animate({ scrollTop: 0 }, "slow");
+        return false;
+    });
+
+});
+})
